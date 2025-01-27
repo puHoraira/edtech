f0a21c58793d0f2c216b8b1f5c4394e5ea0ac0a9
meavi44 <shrabankarmoker-2021911220@cs.du.ac.bd>
2025-01-27 12:41:58 +0600

    updating class

--- lib/liveClass/utils.dart
+++ lib/liveClass/utils.dart
@@ -1,4 +1,4 @@
 class Utils{
-  static int app_id = 398259075;
-  static String app_sign_id = '7e8d2c0dcdb846b1a2397fcdf4511d24175e3d8b34f04d77b2519f5e9c9a21b8';
+  static int app_id = 1152517391;
+  static String app_sign_id = 'd4e7f3ef6abf8a882c081dcc5d13745c5da3bbba6212ad2a4ef3853457dce52a';
 }
